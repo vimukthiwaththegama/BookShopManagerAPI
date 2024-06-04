6587611ba34e97ab9f679e0f0180d777d3af7c9d
VimukthiWaththegama <vwaththegama@gmail.com>
2024-06-04 18:33:18 +0530

    Added security package

---
+++ BookShopManager/src/main/java/com/BookShopManager/BookShopManager/security/ApplicationSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.BookShopManager.BookShopManager.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig  {
+}
