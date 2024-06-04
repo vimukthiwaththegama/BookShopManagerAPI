9dc5c98ce27f64bb48b286ad6c6deeeedfe72890
VimukthiWaththegama <vwaththegama@gmail.com>
2024-06-04 21:44:42 +0530

    changes have commented,need to implement

--- BookShopManager/src/main/java/com/BookShopManager/BookShopManager/security/ApplicationSecurityConfig.java
+++ BookShopManager/src/main/java/com/BookShopManager/BookShopManager/security/ApplicationSecurityConfig.java
@@ -1,10 +1,20 @@
 package com.BookShopManager.BookShopManager.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig  {
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        return httpSecurity.authorizeHttpRequests(registry->{
+            registry.requestMatchers("/home").permitAll();
+            registry.requestMatchers("admin");
+        });
+    }*/
 }
