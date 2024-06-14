35c88a6d9e06355393ed2dc9c7f685e5d119a175
VimukthiWaththegama <vwaththegama@gmail.com>
2024-06-14 14:17:54 +0530

    Added generateToken

--- BookShopManager/src/main/java/com/BookShopManager/BookShopManager/security/JwtService.java
+++ BookShopManager/src/main/java/com/BookShopManager/BookShopManager/security/JwtService.java
@@ -2,13 +2,17 @@ package com.BookShopManager.BookShopManager.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
-import static io.jsonwebtoken.Jwts.*;
 
 @Service
 public class JwtService {
@@ -16,11 +20,30 @@ public class JwtService {
     private static final String SECRET_KEY="DqtEQR12F0VDXG52g24X8TAInFqK5OzNXpgtcbQ+37X831SfLjnIkByiJwHq6RzJ\n";
 
     public String extractUsername(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken,Claims::getSubject);
     }
-    public Claims extractAllClaims(String jwtToken){
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey()).build()
+    public <T> T extractClaim(String jwtToken, Function<Claims,T> claimsResolver) {
+        final Claims claims=extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+    public String generateToken(
+            Map<String,Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + (1000*60*24)))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    private Claims extractAllClaims(String jwtToken){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
                 .parseClaimsJws(jwtToken)
                 .getBody();
     }
